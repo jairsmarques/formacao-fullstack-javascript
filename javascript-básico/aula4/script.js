d4381ee9545bff9b28bce2252f732f237f86a2f2
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-12 20:45:09 -0300

    Adicinando a aula 4 do módulo JavaScript

---
+++ javascript-básico/aula4/script.js
@@ -0,0 +1,15 @@
+//Comentário de uma linha
+
+/*
+Comentário de multiplas linhas
+ */
+
+/*
+Um programa de computador é uma lista de "intruções" a serem "executadas" por um computador.
+
+Um programa JavaScript é uma lista de instruções de programação.
+ */
+
+
+alert('Hello Word!');
+console.log('Hello Word!');
