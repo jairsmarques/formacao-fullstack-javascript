68aa2e466fcc2eb239b60fe97d98b60fbc23685b
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-13 11:17:41 -0300

    Adicionando a aula 8 do módulo JavaScript

---
+++ javascript-básico/aula8/script.js
@@ -0,0 +1,31 @@
+/*
+
+== igualdade de valores
+=== igualdade de valores e tipos
+!== diferença
+
+> maior que
+< menor que
+
+*/
+
+let a = 10
+let b = 5
+
+console.log(a == b)
+
+let c = 5
+let d = '5' //tipo diferente
+console.log(c === d)
+
+let e = 5
+let f = 7
+console.log(e !== f)
+
+let g = 7
+let h = 2
+console.log(g > h)
+
+let i = 7
+let j = 2
+console.log(i < j)
