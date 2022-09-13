415446e386c35675cb70e2afb6b883e7da39f22b
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-12 21:00:08 -0300

    Adicionando a aula 5 do módulo JavaScript

--- javascript-básico/aula4/script.js
+++ javascript-básico/aula4/script.js
@@ -8,8 +8,10 @@ Comentário de multiplas linhas
 Um programa de computador é uma lista de "intruções" a serem "executadas" por um computador.
 
 Um programa JavaScript é uma lista de instruções de programação.
+
+Valores, operadores, expressões, palavras-chave e comentário.
  */
 
 
-alert('Hello Word!');
-console.log('Hello Word!');
+alert('Hello Word!');//statement
+console.log('Hello Word!');//statement
---
+++ javascript-básico/aula5/script.js
@@ -0,0 +1,22 @@
+
+// var, let, const
+
+//String
+//var nome = 'Jair' 
+
+//Number
+//var idade = 47 
+
+
+//let nome = 'Jair'
+//nome = 'Fulano'
+
+//let idade = 47
+
+const nome = 'Jair' //Constante não pode ser alterada
+const sobreNome = 'de Sousa'
+
+const idade = 47
+
+console.log("Meu nome é " + nome + " " + sobreNome)
+console.log("Minha idade é " + idade)
