030b20b4211ee7feaa18fdd512d71ac16efce360
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-13 11:09:45 -0300

    Adicionando a aula 7 do módulo JavaScript

---
+++ javascript-básico/aula7/script.js
@@ -0,0 +1,55 @@
+/*
+
+adição: +
+subtração: -
+multiplicação: *
+divisão: /
+incremento: ++
+decremento: --
+
+*/
+
+
+// adição
+let a = 10
+let b 
+let c = a + b
+console.log(c)
+//NAN = Not a Number
+
+//subtração
+let d = 10
+let e = 5
+let f = d - e
+console.log(f)
+
+//multiplicação
+let g = 5
+let h = 2
+let i = g * h
+console.log(i)
+
+//divisão
+let j = 50
+let l = 10
+let m = j / 10
+console.log(m)
+
+//incremento
+let n = 10
+n++
+console.log(n)
+
+//decremento
+let o = 10
+o--
+console.log(o)
+
+//atribuição
+let p =12
+p += 25 // 12 + 25 = 37
+console.log(p)
+
+let q = 25
+q -= 12 // 25 - 12 = 13
+console.log(q)
