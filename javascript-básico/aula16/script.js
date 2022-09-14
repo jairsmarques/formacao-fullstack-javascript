c30fb4900c2e2e496e3524d0ab3d85529bdc1683
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-14 17:49:01 -0300

    Adicionando a aula 16 do módulo JavaScript

---
+++ javascript-básico/aula16/script.js
@@ -0,0 +1,21 @@
+
+
+/*
+ function soma(num1, num2) {
+ return num1 + num2
+}
+
+let resultado = soma(10, 5)
+
+console.log(resultado)
+document.write('O resultado é:' + resultado)
+ */
+
+function milhasEmKm(milhas) {
+    let km = milhas * 1.6
+    return km
+}
+
+let resultado = milhasEmKm(150)
+document.write(resultado)
+
