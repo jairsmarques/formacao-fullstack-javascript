0a06f82203596d80eaa8d25a53b8596615d84d15
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-14 17:39:36 -0300

    Adicionando a aula 14 do módulo javaScript

---
+++ javascript-básico/aula15/script.js
@@ -0,0 +1,10 @@
+
+
+function escreverNoConsole(mensagem, nome) {
+    console.log(`Olá ${nome}, ${mensagem}`)
+}
+
+escreverNoConsole('Seja muito bem-vindo(a)!', 'Jair')
+
+console.log(parseInt('12'))
+
